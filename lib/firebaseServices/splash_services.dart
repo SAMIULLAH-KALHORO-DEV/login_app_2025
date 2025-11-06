2fd6c990633a9ae2bfd3963d58b86851fa229ee0
Your Name <samiullahabbasy@gmail.com>
2025-11-06 21:31:37 +0500

    next day

--- lib/firebaseServices/splash_services.dart
+++ lib/firebaseServices/splash_services.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:login_app_2025/ui/auth/login_screen.dart';
-import 'package:login_app_2025/ui/firestore/firestore_list_screen.dart';
+import 'package:login_app_2025/ui/dashboard_screens/admin_dashboard.dart';
 // import 'package:login_app_2025/ui/firestore/firestore_list_screen.dart';
 
 class SplashServices {
@@ -15,7 +15,7 @@ class SplashServices {
     if (user != null) {
       Timer(
         Duration(seconds: 3),
-        () => Navigator.push(context, MaterialPageRoute(builder: (context) => FireStoreScreen())),
+        () => Navigator.push(context, MaterialPageRoute(builder: (context) => AdminDashboard())),
       );
     } else {
       Timer(
